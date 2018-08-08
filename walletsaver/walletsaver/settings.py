953613515c7ae1d67ccd9909eb6e7473183399e9
Rogério <rsalmei@gmail.com>
2018-08-08 00:14:12 -0300

    chore(settings) changes configuration to use postgres

--- walletsaver/walletsaver/settings.py
+++ walletsaver/walletsaver/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'walletsaver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wsaver',
+        'USER': 'wsaveruser',
+        'PASSWORD': 'wsaver4pwd',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
