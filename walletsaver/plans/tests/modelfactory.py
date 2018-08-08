bceddb16d4f67f0cdb4dbfebcf7f6ad8420c7254
Rogério <rsalmei@gmail.com>
2018-08-08 05:14:12 -0300

    test(plans) plans model factory

---
+++ walletsaver/plans/tests/modelfactory.py
@@ -0,0 +1,17 @@
+import factory
+from faker import Factory
+
+from plans.models import CarrierPlan
+
+faker = Factory.create()
+
+
+class CarrierPlanFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = CarrierPlan
+
+    carrier = factory.LazyFunction(faker.random_digit)
+    title = factory.LazyFunction(faker.word)
+    description = factory.LazyFunction(faker.text)
+    current_price = factory.LazyFunction(lambda: faker.random_number() / 100)
+    old_price = factory.LazyAttribute(lambda self: self.current_price * 1.1)
